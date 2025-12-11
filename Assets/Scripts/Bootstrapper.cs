8c033c21fa7636678736a18e3b6f132830ce12b9
Striker <stalker2404@list.ru>
2025-12-11 10:27:27 +0300

    Подготовка к созданию EntryPoint

--- Assets/Scripts/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class Bootstrapper : MonoBehaviour
 {
+    public void Start()
+    {
+        Debug.Log("Game Bootstrapper Started");
+        //Инициализация игровых систем
+        //Загружаем персонажа
+        //Загружаем основной уровень
+        //Загружаем сохраненные данные
+        //Инициализируем UI
 
+    }
 }
