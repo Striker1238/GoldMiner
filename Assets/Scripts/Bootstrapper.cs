6683c5e12b7a966c0c85c7c27d6d0080a163212f
Striker <stalker2404@list.ru>
2025-12-19 13:17:40 +0300

    Небольшие модификации

--- Assets/Scripts/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper.cs
@@ -1,6 +1,20 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bootstrapper : MonoBehaviour
 {
+    public string mainSceneName;
+    
 
+    public void Start()
+    {
+        //прогружаем новую сцену
+        SceneManager.LoadScene(mainSceneName,LoadSceneMode.Additive);
+        //создаем игрока
+        //подгружаем сохраненые данные
+        //генерируем первую мапу
+
+
+    }
 }
